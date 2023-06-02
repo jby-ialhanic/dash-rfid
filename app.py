52978b3977e98c30cc4d2644c2802d6dc929f275
Ibrahim Al Hanich <ibrahim.al.hanich@joby.aero>
2023-06-02 11:57:46 -0700

    added random noise to points

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ from collections import deque
 import pandas as pd
 import psycopg2
 import time
+import random
 
 #create dash app
 app = dash.Dash(__name__)
-
 #connect to the db containing our tag coordinates
 conn = psycopg2.connect(
     host = "10.15.72.15",
@@ -34,16 +34,35 @@ app.layout = html.Div([
     )
 ])
 
+#functions
+
+def random_noise(list):
+    return_list = [] #define an empty array
+    for element in list: #cycle through all entries in your original array
+        plusminus = random.randrange(0,2) #this will be used to decide if you're going to subract or add the simulated noise
+        ran_var = random.randrange(0,5) #random noise generation
+        ran_var = ran_var/10
+        if plusminus == 0:
+            element = element + ran_var #add
+        else:
+            element = element - ran_var #subtract
+        return_list.append(element) #store values into empty array
+    return return_list #return the array
+
 def update_DF(): #query the DB to get the latest x,y coordinates for each unique toolId
     cursor = conn.cursor()
     cursor.execute('SELECT public."toolLocation"."toolId", x, y FROM public."toolLocation" JOIN (SELECT "toolId", MAX("ts") AS latest_timestamp FROM public."toolLocation" GROUP BY "toolId") subquery ON public."toolLocation"."toolId" = subquery."toolId" AND public."toolLocation"."ts" = subquery.latest_timestamp ORDER BY public."toolLocation"."toolId" ASC') #order by timestamp descending order
-    column_names = [desc[0] for desc in cursor.description]
-    results = cursor.fetchall()
-    pd_results = pd.DataFrame(results, columns=column_names)
-    return pd_results
+    column_names = [desc[0] for desc in cursor.description] #define the title of each column
+    results = cursor.fetchall() #fetch data from postgres db
+    pd_results = pd.DataFrame(results, columns=column_names) #create a pandas dataframe with the data you received from postgres db
+    ynoise = random_noise(pd_results["y"]) #generate noise for y axis values
+    xnoise = random_noise(pd_results["x"]) #generate noise for x axis values
+    pd_results["y"] = ynoise #overwrite your dataframe
+    pd_results["x"] = xnoise
+    return pd_results #return results
 
 def create_scatterplot(data): #use plotly express to make a scatterplot with collected data and use different colors for each toolId
-    scatterplot = px.scatter(data, x = 'x', y = 'y', color = 'toolId', opacity = 0.1)
+    scatterplot = px.scatter(data, x = 'x', y = 'y', color = 'toolId', opacity = 0.8)
     return scatterplot
 
 @app.callback( #the callback function updates the output when the input changes. We're trying to update the scatterplot whenever a 60s interval
@@ -53,6 +72,7 @@ def create_scatterplot(data): #use plotly express to make a scatterplot with col
     [Input('interval-component', 'n_intervals')]
 )
 def update_scatter_plot(n): #this callback will use this function whenever the input values change, and pass the return into the output
+    # print("update 1")
     latest_data = update_DF()
     fig = create_scatterplot(latest_data)
     return fig
